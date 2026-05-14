456f7bae2354e8d164b8cb628ba7c15759f92018
RybaDeb <kacpergrzegorek07@gmail.com>
2026-05-14 18:21:56 +0200

    detectAbandoned

--- backend/src/main/java/com/ubersim/services/FailureDetector.java
+++ backend/src/main/java/com/ubersim/services/FailureDetector.java
@@ -1,14 +1,32 @@
 package com.ubersim.services;
 
 import com.ubersim.domain.*;
+import com.ubersim.engine.SimulationConfig;
+import com.ubersim.enums.PassengerStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class FailureDetector {
     public List<Passenger> detectAbandoned(List<Passenger> passengers) {
-        return List.of();
+        List<Passenger> abandonedPassengers = new ArrayList<>();
+
+        for(Passenger passenger :passengers){
+            //adds wait time to passengers that are waiting
+            if(passenger.getStatus() == PassengerStatus.WAITING){
+                passenger.incrementWait();
+                //when passenger waits too long, abandons the trip and is added to the list
+                //of passengers that abandoned the trip
+                if(passenger.getMaxWaitTicks()>=passenger.getWaitingTicks()){
+                    passenger.abandon();
+                    abandonedPassengers.add(passenger);
+                }
+
+            }
+        }
+        return abandonedPassengers;
     }
 
     public boolean hasSimulationFailed(SimulationConfig config, SimulationStats stats, int currentTick) {
